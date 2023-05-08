b1de94af98bc2fae5371960df86d3c8c72ab7e80
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-08 12:52:19 -0400

    changed funciton names

--- server.py
+++ server.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/play')
-def level_one():
+def problem_one():
     return render_template("index.html", num=3, color="blue")
 
 @app.route('/play/<int:num>')
-def level_two(num):
+def problem_two(num):
     return render_template("index.html", num=num, color="blue")
 
 @app.route('/play/<int:num>/<string:color>')
-def level_three(num, color):
+def problem_three(num, color):
     return render_template("index.html", num=num, color=color)
 
 if __name__=="__main__":
